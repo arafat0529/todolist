40dc3708f9dd716da1b9ba298a44adc41a04fbb0
arafat0529 <arafat0529@yahoo.com>
2022-09-28 11:11:56 -0700

    database

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,12 +1,16 @@
-const apiRequest = async (url = '', optionsObj = null, errMsg = null) => {
-    try {
-        const response = await fetch(url, optionsObj);
-        if(!response.ok) throw Error('Please reload the app')
-    } catch (error) {
-        errMsg = error.message
-    } finally{
-        return errMsg
-    }
-}
+const apiRequest = async (
+  url = 'https://react-getting-started-f489f-default-rtdb.firebaseio.com/todo',
+  optionsObj = null,
+  errMsg = null
+) => {
+  try {
+    const response = await fetch(url, optionsObj);
+    if (!response.ok) throw Error('Please reload the app');
+  } catch (error) {
+    errMsg = error.message;
+  } finally {
+    return errMsg;
+  }
+};
 
-export default apiRequest
+export default apiRequest;
